f82f0f2d57cfc2e3ab4221abbad324bd38e3d5b2
haran seok <gkfks222@gmail.com>
2023-06-15 13:01:42 +0900

    axios main.ts에 전역으로 설정

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>home</div>
+  <div>
+    <h1>Home</h1>
+    <input type="text" v-model="searchText" @keydown.enter="searchMovies" />
+    <v-btn @click="searchMovies">검색</v-btn>
+    <ul>
+      <!-- <li v-for="movie in movies" :key="movie">{{ movie.Title }}</li> -->
+    </ul>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, inject } from "vue";
+const searchText = ref("");
+const movies = ref([]);
+
+const axios = inject("$axios");
+interface Movie {}
+
+const searchMovies = async () => {
+  let res = await axios.get(
+    `https://www.omdbapi.com?apikey=7035c60c&s=${searchText.value}`
+  );
+  console.log(res);
+};
+</script>
 
 <style lang="scss" scoped></style>
