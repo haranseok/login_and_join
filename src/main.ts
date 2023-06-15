704b956d749f1cf4032ec769253654394c99c7b4
haran seok <gkfks222@gmail.com>
2023-06-15 13:01:58 +0900

    axios 전역 설정

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
-import Router from '@/router/index'
-import i18n from './i18n'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+import Router from "@/router/index";
+import i18n from "./i18n";
+import Axios from "axios";
 
 const app = createApp(App);
 const pinia = createPinia();
 
-loadFonts()
-app.use(i18n)
-app.use(pinia)
-app.use(vuetify)
-app.use(Router)
-app.mount('#app')
+app.provide("$axios", Axios);
+
+loadFonts();
+app.use(i18n);
+app.use(pinia);
+app.use(vuetify);
+app.use(Router);
+app.mount("#app");
