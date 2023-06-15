4eae1a349202fe79b4ad8358d013768c18e096a8
haran seok <gkfks222@gmail.com>
2023-06-15 11:10:16 +0900

    homeb 파일 및 404 Not Found 파일  라우터 설정

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
       //
-    }
+    };
   },
-})
+});
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,20 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      redirect: "/home",
+    },
+    {
+      path: "/home",
+      component: () => import("@/pages/Home.vue"),
+    },
+    {
+      path: "/:notFound(.*)*",
+      component: () => import("@/pages/NotFound.vue"),
+    },
+  ],
 });
 
 export default router;
