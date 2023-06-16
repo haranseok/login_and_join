d1bd8496746637a300270f8bf19c1ce3f8d6aff7
haran seok <gkfks222@gmail.com>
2023-06-16 11:11:27 +0900

    로그인 UI 구현

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="container">
-    <div data-aos="zoom-in" data-aos-easing="ease" data-aos-delay="400">
+    <div data-aos="zoom-in" data-aos-easing="ease" data-aos-delay="250">
       <v-card>
-        <div class="top">
-          <h2>login</h2>
-          <p><strong>admin</strong> dashboard</p>
-        </div>
+        <p><strong>admin</strong> dashboard</p>
         <div class="form_box">
           <input type="text" placeholder="id" />
-          <input type="password" placeholder="password" />
+          <div class="pw_box">
+            <input
+              :type="isShow === true ? 'text' : 'password'"
+              placeholder="password"
+            />
+            <v-icon
+              class="cp"
+              @click="changePwTpe"
+              color="#d3d3d3"
+              :icon="isShow === true ? 'mdi-eye-off' : 'mdi-eye'"
+            />
+          </div>
         </div>
-        <v-btn @click="doLogin">Login</v-btn>
+        <v-btn @click="doLogin" block color="#fec422" rounded="xs">Login</v-btn>
       </v-card>
     </div>
   </div>
@@ -18,17 +26,26 @@
 
 <script lang="ts" setup>
 import { useRouter } from "vue-router";
+import { ref } from "vue";
+
 const router = useRouter();
+const isShow = ref(false);
 
 const doLogin = () => {
   router.push("/home");
+};
+
+const changePwTpe = () => {
+  isShow.value = !isShow.value;
+  console.log(isShow.value);
 };
 </script>
 
 <style lang="scss" scoped>
 input {
   &::placeholder {
-    text-transform: capitalize;
+    font-size: 0.85rem;
+    text-transform: uppercase;
   }
 }
 .container {
@@ -41,20 +58,38 @@ input {
 }
 .v-card {
   width: 500px;
+  padding: 5%;
   border-top: 8px solid #ffd151;
   text-transform: capitalize;
-  .top {
-    padding: 6% 0;
-    text-align: center;
-    h2 {
-      margin-bottom: 3%;
-      font-size: 2rem;
-    }
-    p {
-      strong {
-        color: #fec422;
-      }
+  text-align: center;
+  p {
+    margin: 10% 0;
+    strong {
+      color: #fec422;
     }
   }
+}
+
+.form_box {
+  width: 100%;
+  & > input,
+  .pw_box {
+    width: 100%;
+    padding: 3%;
+    margin: 2% 0;
+    background: rgba(177, 161, 77, 0.055);
+  }
+  .pw_box {
+    display: flex;
+    justify-content: space-between;
+    input {
+      width: 100%;
+    }
+  }
+}
+
+.v-btn {
+  margin: 5% 0;
+  color: #f8f8f8;
 }
 </style>
