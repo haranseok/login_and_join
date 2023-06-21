f2cfadfcde24e5313cb309cf498c7f823e02cece
haran seok <gkfks222@gmail.com>
2023-06-21 18:41:16 +0900

    pages 파일 속 component 별로 scss 파일 분리

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div data-aos="zoom-in" data-aos-easing="ease" data-aos-delay="250">
       <v-card>
-        <p><strong>login</strong> template</p>
+        <p class="title"><strong>login</strong> template</p>
         <div class="form_box">
           <input type="text" placeholder="id" v-model="userId" />
           <div class="pw_box">
@@ -39,12 +39,13 @@
               size="small"
             ></v-btn>
             <v-btn @click="googleLogin" rounded="xs" size="small" icon=""
-              ><img src="@/assets/images/g-logo.png" alt=""
+              ><img class="google_logo" src="@/assets/images/g-logo.png" alt=""
             /></v-btn>
           </div>
         </div>
       </v-card>
     </div>
+    <SignUp v-if="signUpOpen" />
   </div>
 </template>
 
@@ -53,6 +54,8 @@ import { useRouter, useRoute } from "vue-router";
 import { ref, onMounted } from "vue";
 import Axios from "axios";
 import { googleTokenLogin } from "vue3-google-login";
+import SignUp from "@/pages/login/SignUp.vue";
+
 const router = useRouter();
 const isShow = ref(false);
 const route = useRoute();
@@ -64,6 +67,8 @@ const user = ref({
 });
 const userId = ref("");
 const userPw = ref("");
+const signUpOpen = ref(true);
+
 const doLogin = () => {
   router.push("/home");
 };
@@ -121,87 +126,5 @@ const googleLogin = () => {
 </script>
 
 <style lang="scss" scoped>
-input {
-  &::placeholder {
-    font-size: 0.85rem;
-    text-transform: uppercase;
-  }
-}
-.container {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgb(250, 250, 247);
-}
-.v-card {
-  width: 500px;
-  padding: 5%;
-  border-top: 8px solid #ffd151;
-  text-transform: capitalize;
-  text-align: center;
-  p {
-    margin: 10% 0;
-    strong {
-      color: #fec422;
-    }
-  }
-}
-
-.form_box {
-  width: 100%;
-  & > input,
-  .pw_box {
-    width: 100%;
-    padding: 3%;
-    margin: 2% 0;
-    background: rgba(177, 161, 77, 0.055);
-  }
-  .pw_box {
-    display: flex;
-    justify-content: space-between;
-    input {
-      width: 100%;
-    }
-  }
-  .v-btn {
-    margin: 5% 0;
-    color: #f8f8f8;
-  }
-}
-
-.social_box {
-  .line {
-    display: flex;
-    flex-basis: 100%;
-    align-items: center;
-    color: rgba(0, 0, 0, 0.2);
-    font-size: 0.85rem;
-    margin: 2.5rem 0;
-    text-transform: uppercase;
-    color: #aaa;
-    &::before,
-    &::after {
-      content: "";
-      flex-grow: 1;
-      margin: 0px 14px;
-      background: rgba(0, 0, 0, 0.15);
-      height: 1px;
-      line-height: 0px;
-    }
-  }
-  .btn_wrap {
-    display: flex;
-    justify-content: center;
-    .v-btn {
-      margin: 0 5px;
-      color: #624a3d;
-      border-radius: 3px;
-      img {
-        width: 25px;
-      }
-    }
-  }
-}
+@import url("@/style/pages/login.scss");
 </style>
